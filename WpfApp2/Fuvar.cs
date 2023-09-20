2169316a10ca6a9d60d651a4f7f65ebd9b1ab830
orosz.zsombor.oliver <orosz.zsombor.oliver@57-02>
2023-09-20 10:08:04 +0200

    Add project files.

---
+++ WpfApp2/Fuvar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Fuvar
+    {
+        int taxiAzon;
+        string indulasIdopont;
+        int utazasTartam;
+        double megtettTavolsag;
+        double viteldij;
+        double borravalo;
+        string fizetésMod;
+        public Fuvar(string csvSor)
+        {
+            var mezo = csvSor.Split(";");
+            this.taxiAzon = int.Parse(mezo[0]);
+            this.IndulasIdopont = mezo[1];
+            this.utazasTartam = int.Parse(mezo[2]);
+            this.megtettTavolsag = double.Parse(mezo[3]);
+            this.viteldij = double.Parse(mezo[4]);
+            this.borravalo = double.Parse(mezo[5]);
+            this.fizetésMod = mezo[6];
+
+        }
+        public int TaxiAzon { get => taxiAzon; set => taxiAzon = value; }
+        public string IndulasIdopont { get => indulasIdopont; set => indulasIdopont = value; }
+        public int UtazasTartam { get => utazasTartam; set => utazasTartam = value; }
+        public double MegtettTavolsag { get => megtettTavolsag; set => megtettTavolsag = value; }
+        public double Viteldij { get => viteldij; set => viteldij = value; }
+        public double Borravalo { get => borravalo; set => borravalo = value; }
+        public string FizetésMod { get => fizetésMod; set => fizetésMod = value; }
+    }
+
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // << Kecskés Dominik
+            // >> Stiegelmayer Bálint
+
+            
+
+        
+        }
+        
+        private void utazasSzamabtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Fuvar> Fuvarok = new List<Fuvar>();
+            File.ReadAllLines("fuvar.csv").Skip(1).ToList().ForEach(x => new Fuvar(x));
+            int fuvarokSzama = Fuvarok.Count();
+            MessageBox.Show(Convert.ToString(fuvarokSzama));
+        }
+
+        private void tavolsagbtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Fuvar> Fuvarok = new List<Fuvar>();
+            File.ReadAllLines("fuvar.csv").Skip(1).ToList().ForEach(x => new Fuvar(x));
+
+            double osszesKm = Fuvarok.Sum(x => x.MegtettTavolsag);
+            osszesKm = Math.Round( osszesKm * 1.6,2);
+            MessageBox.Show(Convert.ToString(osszesKm));
+        }
+
+        private void leghosszabbbtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Fuvar> Fuvarok = new List<Fuvar>();
+            File.ReadAllLines("fuvar.csv").Skip(1).ToList().ForEach(x => new Fuvar(x));
+
+            Fuvar leghosszabb = Fuvarok.Find(x => x.UtazasTartam == Fuvarok.Max(x => x.UtazasTartam));
+
+            MessageBox.Show(Convert.ToString(leghosszabb));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Fuvar> Fuvarok = new List<Fuvar>();
+            File.ReadAllLines("fuvar.csv").Skip(1).ToList().ForEach(x => new Fuvar(x));
+            try
+            {
+                int fuvarSzam = Convert.ToInt32(adatTxt.Text);
+
+                double bevetel = Fuvarok.Where(x => x.TaxiAzon == fuvarSzam).Sum(x => x.Viteldij + x.Borravalo);
+                MessageBox.Show(Convert.ToString(bevetel));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Egész számot adjon meg");
+            }
+
+        }
+    }
+
+}
